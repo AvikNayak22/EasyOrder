63c45407ae0cf81a898d7f2a1129d9c00512f63b
AvikNayak <nayakavik19@gmail.com>
2025-02-27 20:05:27 +0530

    worked on Greetings and MiniCard ui design

--- pos-frontend/src/pages/Home.jsx
+++ pos-frontend/src/pages/Home.jsx
@@ -1,11 +1,32 @@
+import Greetings from "../components/home/Greetings";
+import MiniCard from "../components/home/MiniCard";
 import BottomNav from "../components/shared/BottomNav";
+
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
 
 const Home = () => {
   return (
     <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* left section */}
+      <div className="flex-[3] ">
+        <Greetings />
 
-      <div className="flex-[3] bg-red-600 "></div>
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title="Total Earnings"
+            icon={<BsCashCoin />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title="In Progress"
+            icon={<GrInProgress />}
+            number={16}
+            footerNum={3.6}
+          />
+        </div>
+      </div>
 
       {/* right section */}
       <div className="flex-[2] bg-blue-600 "></div>
