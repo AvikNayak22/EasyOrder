3ec50304eb8dd0c0d46afb1520df2325302b53a9
AvikNayak <nayakavik19@gmail.com>
2025-03-02 21:58:11 +0530

    worked on cartSlice integration

--- pos-frontend/src/components/menu/CartInfo.jsx
+++ pos-frontend/src/components/menu/CartInfo.jsx
@@ -1,58 +1,75 @@
+import { useRef, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeItem } from "../../redux/slices/cartSlice";
+
 import { FaNotesMedical } from "react-icons/fa";
 import { RiDeleteBin2Fill } from "react-icons/ri";
 
 const CartInfo = () => {
+  const cartData = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({
+        top: scrollRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [cartData]);
+
+  const handleRemove = (itemId) => {
+    dispatch(removeItem(itemId));
+  };
+
   return (
     <div className="px-4 py-2">
       <h1 className="text-lg text-[#e4e4e4] font-semibold tracking-wide">
         Order details
       </h1>
 
-      <div className="mt-4 overflow-y-scroll scrollbar-hide h-[380px] ">
-        <div className="bg-[#1f1f1f] rounded-lg p-4 mb-2">
-          <div className="flex items-center justify-between">
-            <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
-              Chicken Tikka
-            </h1>
-            <p className="text-[#ababab] font-semibold">x2</p>
-          </div>
+      <div
+        ref={scrollRef}
+        className="mt-4 overflow-y-scroll scrollbar-hide h-[380px]"
+      >
+        {cartData.length === 0 ? (
+          <p className="text-[#ababab] text-sm flex justify-center items-center h-[380px]">
+            Your cart is empty. Start adding items!
+          </p>
+        ) : (
+          cartData.map((item) => {
+            return (
+              <div key={item.id} className="bg-[#262626] rounded-lg p-4 mb-2">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
+                    {item.name}
+                  </h1>
+                  <p className="text-[#ababab] font-semibold">
+                    x{item.quantity}
+                  </p>
+                </div>
 
-          <div className="flex items-center justify-between mt-3">
-            <div className="flex items-center gap-3">
-              <RiDeleteBin2Fill
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-              <FaNotesMedical
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-            </div>
-            <p className="text-[#f5f5f5] text-md font-bold">₹123</p>
-          </div>
-        </div>
-        <div className="bg-[#1f1f1f] rounded-lg p-4 mb-2">
-          <div className="flex items-center justify-between">
-            <h1 className="text-[#ababab] font-semibold tracking-wide text-md">
-              Chicken Tikka
-            </h1>
-            <p className="text-[#ababab] font-semibold">x2</p>
-          </div>
-
-          <div className="flex items-center justify-between mt-3">
-            <div className="flex items-center gap-3">
-              <RiDeleteBin2Fill
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-              <FaNotesMedical
-                className="text-[#ababab] cursor-pointer"
-                size={20}
-              />
-            </div>
-            <p className="text-[#f5f5f5] text-md font-bold">₹123</p>
-          </div>
-        </div>
+                <div className="flex items-center justify-between mt-3">
+                  <div className="flex items-center gap-3">
+                    <RiDeleteBin2Fill
+                      onClick={() => handleRemove(item.id)}
+                      className="text-[#ababab] cursor-pointer"
+                      size={20}
+                    />
+                    <FaNotesMedical
+                      className="text-[#ababab] cursor-pointer"
+                      size={20}
+                    />
+                  </div>
+                  <p className="text-[#f5f5f5] text-md font-bold">
+                    ₹{item.price}
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
--- pos-frontend/src/components/menu/MenuContainer.jsx
+++ pos-frontend/src/components/menu/MenuContainer.jsx
@@ -1,12 +1,18 @@
-import { GrRadialSelected } from "react-icons/gr";
-import { menus } from "../../constants";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addItems } from "../../redux/slices/cartSlice";
+
+import { menus } from "../../constants";
+
+import { GrRadialSelected } from "react-icons/gr";
 import { FaShoppingCart } from "react-icons/fa";
 
 const MenuContainer = () => {
+  const dispatch = useDispatch();
   const [selected, setSelected] = useState(menus[0]);
   const [itemCount, setItemCount] = useState(0);
   const [itemId, setItemId] = useState();
+
   const increment = (id) => {
     setItemId(id);
 
@@ -14,12 +20,31 @@ const MenuContainer = () => {
 
     setItemCount((prev) => prev + 1);
   };
+
   const decrement = (id) => {
     setItemId(id);
 
     if (itemCount <= 0) return;
 
     setItemCount((prev) => prev - 1);
+  };
+
+  const handleAddToCart = (item) => {
+    if (itemCount === 0) return;
+
+    const { name, price } = item;
+
+    const newObj = {
+      id: new Date(),
+      name,
+      pricePerQuantity: price,
+      quantity: itemCount,
+      price: itemCount * price,
+    };
+
+    dispatch(addItems(newObj));
+
+    setItemCount(0);
   };
 
   return (
@@ -45,7 +70,7 @@ const MenuContainer = () => {
                   <GrRadialSelected className="text-white" size={20} />
                 )}
               </div>
-              <p className="text-[#ababab] text-sm font-semibold">
+              <p className="text-[#f5f5f5] text-sm font-semibold">
                 {menu.items.length} Items
               </p>
             </div>
@@ -56,36 +81,39 @@ const MenuContainer = () => {
       <hr className="border-[#2a2a2a] border-t-2 mt-4 mx-6" />
 
       <div className="grid grid-cols-3 gap-4 px-10 py-4 w-[100%]">
-        {selected?.items.map((menu) => {
+        {selected?.items.map((item) => {
           return (
             <div
-              key={menu.id}
+              key={item.id}
               className="flex flex-col items-start justify-between p-4 rounded-lg h-[140px] cursor-pointer bg-[#2a2a2a] hover:bg-[#383838]"
             >
               <div className="flex items-start justify-between w-full">
                 <h1 className="text-[#f5f5f5] text-lg font-semibold">
-                  {menu.name}
+                  {item.name}
                 </h1>
-                <button className="bg-[#02ca3a]/30 text-[#fafafa] p-2 rounded-lg">
+                <button
+                  onClick={() => handleAddToCart(item)}
+                  className="bg-[#02ca3a]/30 text-[#fafafa] p-2 rounded-lg"
+                >
                   <FaShoppingCart size={20} />
                 </button>
               </div>
               <div className="flex items-center justify-between w-full">
                 <p className="text-[#f5f5f5] text-lg font-bold">
-                  ₹{menu.price}
+                  ₹{item.price}
                 </p>
                 <div className="flex items-center justify-between bg-[#262626] px-4 py-3 rounded-lg gap-6">
                   <button
-                    onClick={() => decrement(menu.id)}
+                    onClick={() => decrement(item.id)}
                     className="text-[#f6b100] text-2xl"
                   >
                     &minus;
                   </button>
                   <span className="text-white">
-                    {menu.id === itemId ? itemCount : "0"}
+                    {item.id === itemId ? itemCount : "0"}
                   </span>
                   <button
-                    onClick={() => increment(menu.id)}
+                    onClick={() => increment(item.id)}
                     className="text-[#f6b100] text-2xl"
                   >
                     &#43;
